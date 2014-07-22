ef3d1bb72fa526b1589f1b7219961902a89fa8b6
Xavier Coureau <xav.is@2cool4school.fr>
2014-07-22 17:15:26 +0200

    Removed unused use statement

--- DependencyInjection/TakeateaTextingExtension.php
+++ DependencyInjection/TakeateaTextingExtension.php
@@ -7,7 +7,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Takeatea\TextingBundle\DependencyInjection\CompilerPass\TextingCompilerPass;
 
 class TakeateaTextingExtension extends Extension
 {
